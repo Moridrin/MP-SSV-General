47a86565859c559051daf3cec5f7704407d5e0a9
JBerkvens <Account@Moridrin.com>
2016-03-28 16:10:53 -0400

    General and Google Apps moved to seperate plugins.

---
+++ mp-ssv-general.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Plugin Name: SSV General
+* Plugin URI: http://studentensurvival.com/plugins/mp-ssv-google-apps
+* Description: SSV General is the parent plugin for all SSV plugins (they all require this plugin).
+* Version: 1.0
+* Author: Jeroen Berkvens
+* Author URI: http://nl.linkedin.com/in/jberkvens/
+* License: WTFPL
+* License URI: http://www.wtfpl.net/txt/copying/
+*/
+
+include_once "options/options.php";
+
+function mp_ssv_unregister_mp_ssv_general(){
+	if (!is_plugin_active('mp-ssv-frontend-members/mp-ssv-frontend-members.php')) {
+		wp_die('Sorry, but this plugin is required by all SSV plugins. Deactivate all SSV plugins before deactivating this plugin. <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Return to Plugins</a>');
+	}
+}
+register_deactivation_hook( __FILE__, 'mp_ssv_unregister_mp_ssv_general' );
+?>
---
+++ options/options.php
@@ -0,0 +1,14 @@
+<?php
+function mp_ssv_add_mp_ssv_menu() {
+	add_menu_page('MP SSV Options', 'MP-SSV Options', 'manage_options', 'mp_ssv_settings', 'mp_ssv_settings_page');
+	add_submenu_page( 'mp_ssv_settings', 'General', 'General', 'manage_options', 'mp_ssv_settings');
+}
+function mp_ssv_settings_page() {
+	?>
+	<div class="wrap">
+		<h1>MP-SSV General Options</h1>
+	</div>
+	<?php
+}
+add_action('admin_menu', 'mp_ssv_add_mp_ssv_menu', 9);
+?>
