0b67091d33ae17380ee11590a0799b876c092b80
JBerkvens <Account@Moridrin.com>
2016-08-10 00:07:53 +0200

    MP-SSV-General moved to mp-ssv-frontend-members/general and mp-ssv-mailchimp/general.

---
+++ general.php
@@ -0,0 +1,9 @@
+<?php
+if (!function_exists('mp_ssv_initialize_general')) {
+    function mp_ssv_initialize_general()
+    {
+        require_once "functions.php";
+        require_once "options/options.php";
+        require_once "models/FrontendMember.php";
+    }
+}
