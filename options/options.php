954064d5879d55c3e2670a327a9a391acff278e8
JBerkvens <Account@Moridrin.com>
2016-08-19 08:38:28 +0200

    1.1.5

--- options/options.php
+++ options/options.php
@@ -4,10 +4,10 @@ if (!defined('ABSPATH')) {
 }
 function ssv_add_ssv_menu()
 {
-    add_menu_page('MP SSV Options', 'SSV Options', 'manage_options', 'ssv_settings', 'ssv_settings_page');
-    add_submenu_page('ssv_settings', 'General', 'General', 'manage_options', 'ssv_settings');
+	add_menu_page('MP SSV Options', 'SSV Options', 'manage_options', 'ssv_settings', 'ssv_settings_page');
+	add_submenu_page('ssv_settings', 'General', 'General', 'manage_options', 'ssv_settings');
 
-
+}
 function ssv_settings_page()
 {
 	?>
