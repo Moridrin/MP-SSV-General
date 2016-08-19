0a50f8da43520e81f496d9df6f2a77623c66d893
JBerkvens <Account@Moridrin.com>
2016-08-19 08:32:24 +0200

    1.1.5

--- options/options.php
+++ options/options.php
@@ -2,16 +2,20 @@
 if (!defined('ABSPATH')) {
 	exit;
 }
-function mp_ssv_add_mp_ssv_menu() {
-	add_menu_page('MP SSV Options', 'MP-SSV Options', 'manage_options', 'mp_ssv_settings', 'mp_ssv_settings_page');
-	add_submenu_page( 'mp_ssv_settings', 'General', 'General', 'manage_options', 'mp_ssv_settings');
+function ssv_add_ssv_menu()
+{
+	add_menu_page('MP SSV Options', 'SSV Options', 'manage_options', 'ssv_settings', 'ssv_settings_page');
+	add_submenu_page('ssv_settings', 'General', 'General', 'manage_options', 'ssv_settings');
 }
-function mp_ssv_settings_page() {
+
+function ssv_settings_page()
+{
 	?>
 	<div class="wrap">
-		<h1>MP-SSV General Options</h1>
+		<h1>SSV General Options</h1>
 	</div>
 	<?php
 }
-add_action('admin_menu', 'mp_ssv_add_mp_ssv_menu', 9);
+
+add_action('admin_menu', 'ssv_add_ssv_menu', 9);
 ?>
