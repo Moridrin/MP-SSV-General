1cfb2062286d02685b0851b85e33035719b6a9f1
Jeroen Berkvens <Account@Moridrin.com>
2017-05-15 08:51:26 +0200

    General Activation Hook

--- general.php
+++ general.php
@@ -57,5 +57,6 @@ if (!class_exists('mp_ssv_general\SSV_General')) {
 		) $charset_collate;";
         $wpdb->query($sql);
     }
+    register_activation_hook(__FILE__, 'mp_ssv_general_register_plugin');
     #endregion
 }
