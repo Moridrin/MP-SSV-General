ee86d9c57b7fdcd05b9d330abbc8d9b180015806
jberkvens <J.Berkvens@Triptic.nl>
2018-07-26 17:04:24 +0200

    Post Content Modal with Link

--- base/shortcodes/PostContent.php
+++ base/shortcodes/PostContent.php
@@ -2,18 +2,25 @@
 
 namespace mp_general\shortcodes;
 
+use mp_general\base\BaseFunctions;
+
 abstract class PostContent extends Post
 {
     public static function postContent($attributes, $innerHtml)
     {
+        // BaseFunctions::var_export($attributes);
         $postContentPost = self::getPost($attributes, $innerHtml);
         if ($postContentPost !== null) {
             $html = '';
-            if (!isset($attributes['header'])) {
-                $attributes['header'] = $postContentPost->post_title;
-            }
+            $attributes += [
+                'header' => $postContentPost->post_title,
+            ];
             ob_start();
-            ?><h1><?= \mp_general\base\BaseFunctions::escape($attributes['header'], 'html') ?></h1><?php
+            if (isset($attributes['header-url'])) {
+                ?><h1><a href="<?= BaseFunctions::escape($attributes['header-url'], 'url') ?>"><?= BaseFunctions::escape($attributes['header'], 'html') ?></a></h1><?php
+            } else {
+                ?><h1><?= BaseFunctions::escape($attributes['header'], 'html') ?></h1><?php
+            }
             $html .= ob_get_clean();
             global $post;
             $currentPost = $post;
